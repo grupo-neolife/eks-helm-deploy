4e3e83ccf30e52c579f9fdafe9980c0b2505399a
Elison <elison.campos@gmail.com>
2022-10-31 21:03:22 -0300

    (fix) typo in atomic flag

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 aws eks update-kubeconfig --name ${EKS_CLUSTER}
 
-helm upgrade ${RELEASE} ${CHART} --wait --actomic --install --reset-values --namespace ${NAMESPACE} --set image.pullPolicy=Always --set image.tag=${TAG} 
+helm upgrade ${RELEASE} ${CHART} --wait --atomic --install --reset-values --namespace ${NAMESPACE} --set image.pullPolicy=Always --set image.tag=${TAG} 
